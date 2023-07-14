d59f3c24911d0025fd54cd18849ba3084f5c40df
Squ1ggly <squ1gglyzerocool@gmail.com>
2023-07-14 13:07:45 +1000

    CHANGED

---
+++ simple-hash/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+Console.WriteLine("Salt:");
+string hashKey = Console.ReadLine() ?? "";
+Console.WriteLine("Hash This:");
+string password = Console.ReadLine() ?? "";
+Console.WriteLine("Hashed Key:");
+using (HMACSHA256 hmac = new(Encoding.UTF8.GetBytes(hashKey)))
+{
+    byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+    byte[] hashBytes = hmac.ComputeHash(passwordBytes);
+    string hexString = BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+    Console.WriteLine(hexString);
+}
