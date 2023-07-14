f93e63e12f46a5d412f9d5f9392f6e7fdefb7a5a
Squ1ggly <squ1gglyzerocool@gmail.com>
2023-07-14 13:57:02 +1000

    FEAT: Added stuff

--- simple-hash/Program.cs
+++ simple-hash/Program.cs
@@ -1,15 +1,55 @@
 ﻿using System.Security.Cryptography;
 using System.Text;
 
-Console.WriteLine("Salt:");
-string hashKey = Console.ReadLine() ?? "";
-Console.WriteLine("Hash This:");
-string password = Console.ReadLine() ?? "";
-Console.WriteLine("Hashed Key:");
+string fullPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\simple-hash-salt.key";
+string hashKey = "";
+bool keySaved = false;
+
+static string getInputFromUser(string question)
+{
+    Console.Write(question);
+    return Console.ReadLine() ?? "";
+}
+
+string useNewKey = getInputFromUser("Use new key yes/no?\n> ");
+if (useNewKey.ToLower().Trim() == "no")
+{
+    try
+    {
+        hashKey = File.ReadAllText(fullPath).Trim() ?? "";
+        keySaved = true;
+    }
+    catch
+    {
+        Console.WriteLine("No key found!");
+    }
+}
+
+if (hashKey == "")
+{
+    hashKey = getInputFromUser("Salt\n> ");
+}
+
+string password = getInputFromUser("Hash This\n> ");
+
+Console.Write("Hashed Key\n> ");
 using (HMACSHA256 hmac = new(Encoding.UTF8.GetBytes(hashKey)))
 {
     byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
     byte[] hashBytes = hmac.ComputeHash(passwordBytes);
     string hexString = BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
     Console.WriteLine(hexString);
+}
+
+if (!keySaved)
+{
+    Console.Write("Save secret yes/no?\n> ");
+    string yesNo = Console.ReadLine() ?? "";
+    if (yesNo.ToLower().Trim() == "yes")
+    {
+        using (StreamWriter writer = new(fullPath))
+        {
+            writer.WriteLine(hashKey);
+        }
+    }
 }
